30dc1ae3bebf4f4ecfd17ad58aa3ecdcd47fe3fa
Yash Rajput <yashrajput7455@gmail.com>
2024-06-27 22:36:58 +0530

    Done

---
+++ Sprint 16/Day1/Q1.js
@@ -0,0 +1,10 @@
+var sp = 1
+var ep = 50
+
+while(sp <= ep){
+
+  console.log(sp)
+
+  sp++
+  
+}
---
+++ Sprint 16/Day1/Q2.js
@@ -0,0 +1,9 @@
+var sp = 1
+
+while(sp <= 10){
+
+  console.log("5*"+sp, "=",5*sp)
+
+  sp++
+  
+}
